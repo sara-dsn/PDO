798db6ef3d19d40899cfff6746f44929af95a964
sara-dsn <sabr.tdg@gmail.com>
2024-01-04 10:40:42 +0100

    x

--- liste.php
+++ liste.php
@@ -35,7 +35,7 @@
 if ($tableau){ 
     foreach ($tableau as $disc){ 
     ?>
-<div  class="card  col-3 mr-4 ml-4 mb-4" style="width: 18rem;">
+<div  class="card border-dark col-3 mr-4 ml-4 mb-4" style="width: 18rem;">
   <img class="card-img-top mt-2" src="img/<?=$disc->disc_picture?>" alt="vinyle">
   <div class="card-body">
     <h5 class="card-title font-weight-bold"><?= $disc->disc_title ?></h5>
